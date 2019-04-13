a59aa7573f1e7e67f99f4e46a1bbe2bd445f14c2
Tanner Ellenberger <49576287+tannerellenberger@users.noreply.github.com>
2019-04-12 23:15:04 -0400

    Add files via upload

---
+++ Program 4/Program 4/LibraryBook.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4
+{
+    using static System.Console;
+
+    public class LibraryBook
+    {
+        private string _title; //title of book
+        private string _author; //author of book
+        private string _publisher; //publisher of book
+        private int _copyrightYear; //copyright year of book
+        private string _callNumber; //call number of book
+        private bool checkedOut; // determines whether book is checked out or not
+
+        // read only property, gets title of book
+        public string Title
+        {
+            get { return _title; } //Precondition: none
+            set { _title = value; } //Postcondition: sets the value of title
+        }
+        
+        // read only property, gets the author of the book
+        public string Author
+        {
+            get { return _author;  } //Precondition: none
+            set { _author = value;  } //Postcondition: sets the value for author
+        }
+       
+        // read only property, gets publisher of book
+        public string Publisher
+        {
+            get { return _publisher;  } //Precondition: none
+            set { _publisher = value; } //Postcondition: sets the value for the publisher
+        }
+        
+        // read only property, gets copyright year, or assigns 2017 if non-negative value
+        public int CopyrightYear
+        {
+
+            get
+            {
+                return _copyrightYear; //Precondition: must be non negative
+            }
+            set
+            {
+                if (value >= 0) //if statement to make sure it is non negative
+                    _copyrightYear = value; //Postcondition: if non negative integer, sets value for copyright year
+                else
+                    _copyrightYear = 2017; //assign 2017 if negative input 
+            }
+        }
+
+        // read only property, gets the call number 
+        public string CallNumber
+        {
+            get { return _callNumber; } //Precondition: none
+            set { _callNumber = value; } //Postcondition: sets value for call number
+        }
+
+
+
+        // five-parameter constructor
+        //Precondition: input of 5 values
+        //Postcondition: assigns values to new variables
+
+        public LibraryBook(string title, string author, string publisher, int copyrightYear,
+                           string callNumber)
+        {
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+            CopyrightYear = copyrightYear;
+            CallNumber = callNumber;
+        }
+        // end of constructor
+
+         
+        //Precondition: none
+        //Postcondition: returns value true to checkedOut
+        public void CheckOut()
+        {
+            checkedOut = true;
+            
+        }
+        //Precondition:none
+        //Postcondition: returns value false
+
+        public void ReturnToShelf()
+        {
+
+            checkedOut = false;
+            
+        }
+        //Precondition: none
+        //Postcondition: returns the checkedOut value, depending on the above methods
+        public bool IsCheckedOut()
+        {
+            
+           return checkedOut;
+            
+        }
+
+        //Precondition: must have inputs
+        //Postcondition: writes string of input values
+
+        //return string of LibraryBook object
+        public override string ToString()
+        {
+            string product;
+
+         product = $"Title:            {Title}  {Environment.NewLine}" +
+                   $"Author:           {Author} {Environment.NewLine}" +
+                   $"Publisher:        {Publisher}  {Environment.NewLine}" +
+                   $"Copyright Year:   {CopyrightYear}  {Environment.NewLine}" +
+                   $"Call Number:      {CallNumber}  {Environment.NewLine}" +
+                   $"Check out status: {checkedOut} {Environment.NewLine}";
+
+            return product;
+        } //end of ToString
+    }//end of class
+
+}//end of program
---
+++ Program 4/Program 4/Program.cs
@@ -0,0 +1,100 @@
+﻿//A7793
+//December 5th, 2017
+//CIS 199-01
+//Program 4
+//Program lets user see what books are checked out or not
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4
+{
+    using static System.Console;
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            // derived class objects
+
+            LibraryBook book1 =
+                new LibraryBook("To Kill a Mockingbird", "Harper Lee", "J. B. Lippincott & Co.", 1964, "BC");
+            LibraryBook book2 =
+                new LibraryBook("Where the Red Fern Grows", "Wilson Rawls", "Doubleday", 1961, "LC");
+            LibraryBook book3 =
+                new LibraryBook("The Alchemist", "Paulo Coelho", "HarperTorch", 1990, "PC");
+            LibraryBook book4 =
+                new LibraryBook("Candide", "Voltaire", "Cramer", 1820, "OC");
+            LibraryBook book5 =
+                new LibraryBook("The Stranger", "Albert Camus", "Camus", 1984, "HC");
+
+
+            //create array for 5 objects
+            LibraryBook[] books = new LibraryBook[5];
+
+
+            //initialize array with objects
+            books[0] = book1;
+            books[1] = book2;
+            books[2] = book3;
+            books[3] = book4;
+            books[4] = book5;
+
+            // ----------------------------------------------------------------------------------
+            //Dr. Wright I had trouble constructing a method for these tests. 
+            //Instead I tried doing it without a method, because I had an overload error.
+            //My apologies for leaving the method out, I couldn't get it to run otherwise.
+            // ----------------------------------------------------------------------------------
+
+            WriteLine("Before: ");
+            WriteLine();
+        
+        
+            // for each loop to assign arrays to the ToString
+            foreach (LibraryBook currentBook in books)
+            {
+                Console.WriteLine(currentBook); //implicitly calls ToString method
+            } //end foreach 
+
+            WriteLine("After: ");
+            WriteLine();
+
+            // data manipulation for 2nd test
+
+            book1.Publisher = "Grant"; 
+            book1.CheckOut(); //show that book is checked out
+            book2.Publisher = "John";
+            book3.Publisher = "Max";
+            book4.Publisher = "Bad";
+            book5.Publisher = "Tim";
+            book5.CheckOut(); //show that book is checked out
+
+
+            // for each loop to assign arrays to the ToString
+            foreach (LibraryBook currentBook in books)
+            {
+                Console.WriteLine(currentBook); //implicitly calls ToString method
+            } //end foreach 
+
+
+            WriteLine("Books that are currently checked out: ");
+            WriteLine();
+            WriteLine();
+            WriteLine();
+
+            WriteLine(book1); //displays the books that are checked out
+            WriteLine(book5); // same as above
+            
+
+        } //end main  
+        
+    } //end progran
+}
